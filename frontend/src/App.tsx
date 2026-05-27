3b00faf6acff85f2c4a4f220527d37c96dee6e61
Kalana-methsara <kalanamethsara53@gmail.com>
2026-05-27 23:10:13 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { STORAGE_KEYS } from "./utils/storageKeys";
+// import { setCredentials, setLoading } from "./features/auth/authSlice";
+// import type { AuthUser } from "./types";
+// import Router from "./router/Router";
+
+function App() {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     const token = localStorage.getItem(STORAGE_KEYS.accessToken);
+//     const userStr = localStorage.getItem(STORAGE_KEYS.user);
+
+//     if (token && userStr) {
+//       try {
+//         dispatch(setCredentials(JSON.parse(userStr) as AuthUser));
+//       } catch {
+//         dispatch(setLoading(false));
+//       }
+//     } else {
+//       dispatch(setLoading(false));
+//     }
+//   }, [dispatch]);
+
+  // return <Router />;
+}
+
+export default App;
