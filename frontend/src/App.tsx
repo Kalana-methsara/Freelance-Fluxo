a44be18ddf7f19729b900f32d7b24548c6a8969b
Kalana-methsara <kalanamethsara53@gmail.com>
2026-05-28 22:26:21 +0530

    implement authentication flow with Redux and add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,29 @@
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { STORAGE_KEYS } from "./utils/storageKeys";
-// import { setCredentials, setLoading } from "./features/auth/authSlice";
-// import type { AuthUser } from "./types";
-// import Router from "./router/Router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { STORAGE_KEYS } from "./utils/storageKeys";
+import { setCredentials, setLoading } from "./features/authSlice";
+import type { AuthUser } from "./types";
+import Router from "./router/Router";
 
 function App() {
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     const token = localStorage.getItem(STORAGE_KEYS.accessToken);
-//     const userStr = localStorage.getItem(STORAGE_KEYS.user);
+  useEffect(() => {
+    const token = localStorage.getItem(STORAGE_KEYS.accessToken);
+    const userStr = localStorage.getItem(STORAGE_KEYS.user);
 
-//     if (token && userStr) {
-//       try {
-//         dispatch(setCredentials(JSON.parse(userStr) as AuthUser));
-//       } catch {
-//         dispatch(setLoading(false));
-//       }
-//     } else {
-//       dispatch(setLoading(false));
-//     }
-//   }, [dispatch]);
+    if (token && userStr) {
+      try {
+        dispatch(setCredentials(JSON.parse(userStr) as AuthUser));
+      } catch {
+        dispatch(setLoading(false));
+      }
+    } else {
+      dispatch(setLoading(false));
+    }
+  }, [dispatch]);
 
-  // return <Router />;
+  return <Router />;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import { store } from "./redux/store.ts"
 createRoot(document.getElementById("root")!).render(
  <StrictMode>
    <Provider store={store}>c
-     {/* <App /> */}
+     <App />
    </Provider>
  </StrictMode>
 )
